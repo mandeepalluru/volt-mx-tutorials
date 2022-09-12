54abad7a3f690f6166bdb04132dfdd2bd6a9c046
Mandeep Alluru <mandeep-allu@hcl.com>
2022-09-12 19:08:21 +0530

    Modified workflow to deploy off test_branch

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export const isDev = process.env.NODE_ENV !== 'production';
 export const DEV_PORT = 3200;
-export const BASE_BRANCH = 'master';
+export const BASE_BRANCH = 'test_branch';
 
-export const HOST_URL = `https://opensource.hcltechsw.com`;
+export const HOST_URL = `https://mandeepalluru.github.io`;
 export const BASE_PATH_URL = 'volt-mx-tutorials';
 
 export const GITHUB_NAME = 'HCL-TECH-SOFTWARE';
