0dece75e08e7349cef7a7d4a6810cd073a324161
Mandeep Alluru <mandeep-allu@hcl.com>
2022-09-12 21:44:50 +0530

    Changed GitHub user id

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,6 @@ export const BASE_BRANCH = 'test_branch';
 export const HOST_URL = `https://mandeepalluru.github.io`;
 export const BASE_PATH_URL = 'volt-mx-tutorials';
 
-export const GITHUB_NAME = 'HCL-TECH-SOFTWARE';
+export const GITHUB_NAME = 'mandeepalluru';
 
 export const SERVER = isDev ? `http://localhost:${DEV_PORT}` : `${HOST_URL}/${BASE_PATH_URL}`;
